c5adc01b8b128e33c8000358601f8872cdeeba5e
Rikko <bulletbill51@yahoo.com>
2024-06-11 15:12:37 -0700

    Polished form appearance

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -5,12 +5,18 @@ function Contact () {
         <div id={Style.contact}>
             <h2>Contact Me</h2>
             <form>
-                <label>Name</label>
-                <input type='text'/>
-                <label>Email</label>
-                <input type='email'/>
-                <label>Message</label>
-                <textarea/>
+                <div className={Style.formSec}>
+                    <label>Name</label>
+                    <input type='text'/>
+                </div>
+                <div className={Style.formSec}>
+                    <label>Email</label>
+                    <input type='email'/>
+                </div>
+                <div className={Style.formSec} id={Style.contact_message}>
+                    <label>Message</label>
+                    <textarea/>
+                </div>
                 <button type='submit'>Submit</button>
             </form>
         </div>
