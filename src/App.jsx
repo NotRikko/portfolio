d2ad1ab9eb42d9e7fb1de31de12cc0526fbb8dab
Rikko <bulletbill51@yahoo.com>
2024-06-11 13:10:29 -0700

    Contact section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import Intro from './Pages/Intro'
 import About from './Pages/About'
 import Projects from './Pages/Projects'
+import Contact from './Pages/Contact'
 import { useRef } from 'react'
 import './App.css'
 
 function App() {
   const aboutRef = useRef(null);
   const projectsRef = useRef(null);
-
+  const contactRef = useRef(null);
   return (
     <>
       <Intro 
         scrollToAbout={() => aboutRef.current.scrollIntoView({ behavior: 'smooth' })}
         scrollToProjects={() => projectsRef.current.scrollIntoView({ behavior: 'smooth' })}
+        scrollToContact={() => contactRef.current.scrollIntoView({behavior: 'smooth'})}
       />
       <div ref={aboutRef}>
         <About />
       </div>
       <div ref={projectsRef}>
         <Projects />
+      </div>
+      <div ref={contactRef}>
+          <Contact />
       </div>
     </>
   )
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,19 @@
+import Style from './Pages.module.css'
+
 function Contact () {
     return (
-        <>
-        </>
+        <div id={Style.contact}>
+            <h2>Contact Me</h2>
+            <form>
+                <label>Name</label>
+                <input type='text'/>
+                <label>Email</label>
+                <input type='email'/>
+                <label>Message</label>
+                <textarea/>
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
     )
 }
 
